099a173a32324d3b7fa2affc7a7f6461b02a2a11
blesst102 <blesst102@gmail.com>
2021-04-05 18:25:19 +0000

    landing page styling

---
+++ app/models/course.rb
@@ -0,0 +1,8 @@
+class Course < ApplicationRecord
+    validates :title, presence: true
+    validates :description, presence: true
+    def to_s
+        title
+    end
+    has_rich_text :description
+end
